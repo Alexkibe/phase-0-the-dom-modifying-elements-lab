cb0ad80fea8f71f96b4816d5ec5e4e7d5eecbf71
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-04-01 23:15:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const toa = document.getElementById('main')
+toa.remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+
+
+
+  
+
